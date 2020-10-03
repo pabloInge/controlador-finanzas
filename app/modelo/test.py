9c441284e97bf1124911daf4f756b5bb7187b80e
Pablo Ingegnieri <pablo.ingegnieri@alumnos.ispi4038.edu.ar>
2020-10-03 19:51:59 -0300

    Correccion testeo de registro tipo

--- app/modelo/test.py
+++ app/modelo/test.py
@@ -1,18 +1,31 @@
 import unittest
-from .tipo_transaccion import ServiceTipoTransaccion, TipoTransaccionDTO, NombreError
-
-
-service = ServiceTipoTransaccion()
+from .tipo_transaccion import (
+    ServiceTipoTransaccion,
+    TipoTransaccionDTO,
+    NombreError,
+    TipoTransaccion,
+    Session,
+)
 
 
 class Test(unittest.TestCase):
+    def setUp(self):
+        self.service = ServiceTipoTransaccion()
+        session = Session()
+        self.tipos = [tipo for tipo in session.query(TipoTransaccion)]
+
     def test_registrar_tipo(self):
         tipo = TipoTransaccionDTO("Envio", "tipo de prueba")
-        self.assertIsNone(service.registrar_tipo(tipo))
+        self.service.registrar_tipo(tipo)
 
+        session = Session()
+        nuevo_tipo = session.query(TipoTransaccion)[-1]
+        self.assertNotEqual(nuevo_tipo.id, self.tipos[-1].id)
+
+    def test_error_nombre(self):
         with self.assertRaises(NombreError):
             tipo = TipoTransaccionDTO("", "tipo de prueba")
-            self.assertIsNone(service.registrar_tipo(tipo))
+            self.service.registrar_tipo(tipo)
 
 
 if __name__ == "__main__":
